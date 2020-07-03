52f5b9bae2f8880119bb266cf5ac576da4d8038a
Nick Fox-Gieg <nick@fox-gieg.com>
2020-07-03 10:09:46 -0400

    first

---
+++ rpi/egl/fullscreen/main.cpp
@@ -0,0 +1,23 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main() {
+
+#ifdef TARGET_OPENGLES
+	ofGLESWindowSettings settings;
+	settings.glesVersion = 2;
+	settings.windowMode = OF_FULLSCREEN;
+	ofCreateWindow(settings);
+#else
+	ofGLWindowSettings settings;
+	settings.windowMode = OF_FULLSCREEN;
+	ofCreateWindow(settings);
+#endif
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp(new ofApp());
+
+}
