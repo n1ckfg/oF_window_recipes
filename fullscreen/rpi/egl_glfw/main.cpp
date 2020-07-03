561f178ae6755d50880cfe2bb353c7b38e58490a
Nick Fox-Gieg <nick@fox-gieg.com>
2020-07-03 10:40:08 -0400

    more egl options

---
+++ fullscreen/rpi/egl_glfw/main.cpp
@@ -0,0 +1,25 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main() {
+
+#ifdef TARGET_OPENGLES
+	ofGLESWindowSettings settings;
+	settings.glesVersion = 2;
+	settings.windowMode = OF_FULLSCREEN;
+	ofCreateWindow(settings);
+#else
+        ofGLFWWindowSettings settings;
+        settings.glesVersion = 2;
+        settings.windowMode = OF_FULLSCREEN;
+        settings.numSamples = 0;
+        ofCreateWindow(settings);                       // <-------- setup the GL context
+#endif
+
+        // this kicks off the running of my app
+        // can be OF_WINDOW or OF_FULLSCREEN
+        // pass in width and height too:
+        ofRunApp( new ofApp());
+
+}
